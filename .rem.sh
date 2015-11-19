a4f589e1ec78f3a0b808a36db75762a982ba531f
Alex Warrington <awarrington0895@gmail.com>
2015-11-19 15:22:07 -0600

    Added the .bashrc for saving and improved the .rem.sh script

--- .rem.sh
+++ .rem.sh
@@ -3,7 +3,7 @@
 : '
 Author: Alex Warrington
 Created: 2015-10-14 13:52:13
-Modified: 2015-10-15 15:34:17
+Modified: 2015-11-19 15:20:02
 Filename: .rem.sh
 '
 
@@ -13,5 +13,44 @@ Filename: .rem.sh
 
 # The trash directory can be cleaned using the "clean" command 
 
-mv $1 ~/.trash
 
+
+if [ $# == 0 ] 
+    then
+        echo "Usage: trash -l | -p | {filename}*"
+    elif [[ "$1" == "-l" ]] 
+        then
+            if [ -d "$HOME/.trash" ]
+                then
+                    ls $HOME/.trash
+                else
+                    echo "There are no files in trash"
+            fi
+    elif [[ "$1" == "-p" ]]
+        then
+            if [ -d "$HOME/.trash" ] 
+                then
+                    rm -r $HOME/.trash
+                else
+                    echo "Directory $HOME/.trash doesn't exist"
+            fi
+    else
+        for i in $@
+            do
+                if [ -f "$i" ]
+                    then        
+                        if [ -d "$HOME/.trash" ]
+                            then
+                                mv $i $HOME/.trash
+                            else
+                                mkdir $HOME/.trash
+                                mv $i $HOME/.trash
+                        fi
+                     else
+                        echo "File $i doesn't exist"
+                fi
+        done
+                                
+        
+        
+fi
--- .update.sh
+++ .update.sh
@@ -3,12 +3,13 @@
 : '
 Author: Alex Warrington
 Created: 2015-10-14 13:52:14
-Modified: 2015-10-21 16:05:58
+Modified: 2015-11-18 08:58:17
 Filename: .update.sh
 '
 
 # This script should be ran from a crontab or similar utility to be ran every 5-10 minutes
 
+# This script checks the file's mod date against what the system has and changes it automatically
 
 ####### SCRIPT REQUIREMENTS ############
 
@@ -26,7 +27,8 @@ readarray commands < tmp.txt
 # For loop that cycles through all the scripts and checks their modify date and changes them if needed 
 for file in "${commands[@]}"
 do
-    
+    # grabs the system's mod date through the stat command and gets the file's internal mod date 
+    # and stores them
     sys_mod_date="$(date --reference=$HOME/bin/$file +"%Y-%m-%d %H:%M:%S")"
     file_mod_date="$(cat $HOME/bin/$file | grep "Modified: [0-9]" | sed "s|Modified: \(.*\)|\1|")"
     if [[ $file_mod_date !=  $sys_mod_date ]]; then
